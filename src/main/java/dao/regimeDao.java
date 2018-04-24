5b7f13210e9504e182b5ca986bd5ff54311cc3db
Antoine Morel <antoine.morel1@grenoble-inp.fr>
2018-04-24 18:02:06 +0200

    C BON LOL

--- src/main/java/dao/regimeDao.java
+++ src/main/java/dao/regimeDao.java
@@ -25,7 +25,7 @@ public class regimeDao {
     
     public void ajoutRegime(String regime) throws SQLException {
         try (Connection c = ds.getConnection()) {
-            PreparedStatement p = c.prepareStatement("INSERT INTO TREGIME VALUES ('?')");
+            PreparedStatement p = c.prepareStatement("INSERT INTO TREGIME VALUES (?)");
             p.setString(1, regime);
             ResultSet rs = p.executeQuery();
         } catch (SQLException e) {
